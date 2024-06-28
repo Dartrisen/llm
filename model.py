e2a66f67227d7514a56cfb6b92be48efcc1fdf67
dartrisen <calculating.scientist@gmail.com>
2024-06-28 18:09:15 +0200

    model.py has been added

---
+++ model.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+torch.manual_seed(1337)
+
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
+
+        return logits
