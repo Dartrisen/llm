95f2c92f84ba77bbdba1ce2d6d290237b9fd81f4
dartrisen <calculating.scientist@gmail.com>
2024-07-02 15:12:20 +0200

    forward method has been added

--- model.py
+++ model.py
@@ -12,6 +12,19 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
 
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        # compute affinities
+        wei = q @ k.transpose(-2, -1) * C**(-0.5)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)
+        # aggregation of the values
+        v = self.value(x)
+        out = wei @ v
+        return out
+
 
 class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size, block_size, n_embd):
